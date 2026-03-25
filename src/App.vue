f8863cbe39c38c4ae8e07d04657cbf4461fa22fc
firu11 <firu@firu.dev>
2026-03-25 15:03:44 +0100

    add alpha tag

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <main>
+    <span id="alpha">Alpha version</span>
     <RouterView />
   </main>
 </template>
@@ -9,5 +10,18 @@
 <style scoped>
 main {
   height: 100%;
+}
+
+#alpha {
+  position: absolute;
+  top: 1rem;
+  left: calc(var(--sidebar-width) + 1rem);
+  padding: 0.1rem 0.3rem;
+
+  font-size: 0.7rem;
+  font-weight: bold;
+  color: var(--git-color);
+  border: 1px solid var(--git-color);
+  border-radius: 2rem;
 }
 </style>
