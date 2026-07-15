f3e6922db2e3173f369b9bc664dbea27a5ef4efe
firu11 <firu@firu.dev>
2026-07-15 13:26:46 +0200

    sidebar toggle button state

--- src/components/SidebarCloseBtn.vue
+++ src/components/SidebarCloseBtn.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { useSidebar } from '@/composables/useSidebar';
-import { FiSidebar } from 'vue-icons-plus/fi';
+import { TbLayoutSidebar, TbLayoutSidebarFilled } from 'vue-icons-plus/tb';
 
 const sidebar = useSidebar();
 </script>
 
 <template>
-  <button id="sidebar-toggle" @click="sidebar.toggle">
-    <FiSidebar />
+  <button id="sidebar-toggle" @click="sidebar.toggle" style="display: flex;">
+    <TbLayoutSidebarFilled style="scale: 1.15;" v-show="sidebar.isOpen.value"/>
+    <TbLayoutSidebar style="scale: 1.15;" v-show="!sidebar.isOpen.value"/>
   </button>
 </template>
 
